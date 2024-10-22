6861675306fd68536f7f5a90922b181f4e4a94a2
alicepgrd <pougeardalice@gmail.com>
2024-10-22 15:44:25 +0200

    composant PokemonCard est dynamique et s'adapte aux données des Pokémons dans pokemonList

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,29 @@
 function PokemonCard() {
+  const pokemon = pokemonList[0];
   return (
     <div>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="imagecarte"
-        ></img>
-        <figcaption>"Bulbasaur"</figcaption>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt="imagecarte"></img>
+        ) : (
+          <p>???</p>
+        )}
+
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     </div>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default PokemonCard;
