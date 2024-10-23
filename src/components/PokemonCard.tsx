87fca3b05cfd6a1c31460db66fecd8dc4df35d22
alicepgrd <pougeardalice@gmail.com>
2024-10-23 10:24:24 +0200

    modifications pour que le composant App envoie les données d'un pokémon au composant PokemonCard via la prop pokemon

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,4 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
   return (
     <div>
       <figure>
@@ -14,16 +13,5 @@ function PokemonCard() {
     </div>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
